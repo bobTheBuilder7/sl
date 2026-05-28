c811525ce887f3d1790b5c62f903718c8a8ff347
Suren Adzhamoglyan <adgsuren@gmail.com>
2026-05-27 23:03:40 -0700

    looks good

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
-export function add(a: number, b: number): number {
-	return a + b;
+interface PageVisit {
+	visitor_id: string;
+	referer: string;
+	href: string;
+	ip: string;
 }
 
-export class MyMath {
-	add(a: number, b: number): number {
-		return a + b;
+interface PageEvent {
+	visitor_id: string;
+	event: string;
+	href: string;
+	ip: string;
+}
+
+export class SLClient {
+	private baseUrl: string;
+
+	constructor(baseUrl: string) {
+		this.baseUrl = baseUrl;
 	}
 
-	subtract(a: number, b: number): number {
-		return a - b;
+	async pageVisit(b: PageVisit) {
+		try {
+			await fetch(`${this.baseUrl}/api/slytics/visit`, {
+				method: "POST",
+				body: JSON.stringify(b),
+			});
+		} catch {}
 	}
 
-	multiply(a: number, b: number): number {
-		return a * b;
+	async pageEvent(b: PageEvent) {
+		try {
+			await fetch(`${this.baseUrl}/api/slytics/event`, {
+				method: "POST",
+				body: JSON.stringify(b),
+			});
+		} catch {}
 	}
 }
