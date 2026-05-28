41e7a6d3f9d4be0c5c9794a879ad972ac24d6210
Suren Adzhamoglyan <adgsuren@gmail.com>
2026-05-27 17:23:48 -0700

    initial

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export function add(a: number, b: number): number {
+	return a + b;
+}
+
+export class MyMath {
+	add(a: number, b: number): number {
+		return a + b;
+	}
+
+	subtract(a: number, b: number): number {
+		return a - b;
+	}
+
+	multiply(a: number, b: number): number {
+		return a * b;
+	}
+}
