1ab50690d899a99971783da86ec29bffce33eb48
Suren Adzhamoglyan <adgsuren@gmail.com>
2026-08-20 17:14:36 -0700

    chore: bump version to 0.0.11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/** @typedef {'hy' | 'ru' | 'en'} Locale */
+
+/**
+ * @typedef {{
+ * 	doctor: { id: number; name: string; position: string };
+ * 	contents: { body: string; lvl: number }[];
+ * }} DoctorResponse
+ */
+
 /** Client for sending page visits and events to the SL analytics API. */
 export class SLClient {
 	#baseUrl;
@@ -10,33 +19,45 @@ export class SLClient {
 	/**
 	 * @param {string} path
 	 * @param {Record<string, unknown>} payload
-	 * @returns {Promise<void>}
+	 * @returns {Promise<unknown>}
 	 */
-	async post(path, payload) {
-		try {
-			const response = await fetch(`${this.#baseUrl}${path}`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(payload),
-			});
-		} catch {}
+	async #post(path, payload) {
+		const req = await fetch(`${this.#baseUrl}${path}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(payload),
+		});
+
+		if (!req.ok) {
+			throw new Error(await req.text());
+		}
+
+		const resp = await req.json();
+
+		return resp;
 	}
 
 	/**
 	 * @param {string} path
 	 * @returns {Promise<void>}
 	 */
-	async get(path, payload) {
-		try {
-			const response = await fetch(`${this.#baseUrl}${path}`, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			});
-		} catch {}
+	async #get(path, payload) {
+		const req = await fetch(`${this.#baseUrl}${path}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!req.ok) {
+			throw new Error(await req.text());
+		}
+
+		const resp = await req.json();
+
+		return resp;
 	}
 
 	/**
@@ -44,7 +65,9 @@ export class SLClient {
 	 * @returns {Promise<void>}
 	 */
 	async pageVisit(payload) {
-		await this.post('POST', '/api/slytics/visit', payload);
+		try {
+			await this.#post('/api/slytics/visit', payload);
+		} catch {}
 	}
 
 	/**
@@ -52,6 +75,18 @@ export class SLClient {
 	 * @returns {Promise<void>}
 	 */
 	async pageEvent(payload) {
-		await this.post('POST', '/api/slytics/event', payload);
+		try {
+			await this.#post('/api/slytics/event', payload);
+		} catch {}
+	}
+
+	/**
+	 * @param {{ id: number; locale: Locale }} params
+	 * @returns {Promise<DoctorResponse>}
+	 */
+	async getDoctor({ id, locale }) {
+		const doctor = await this.#post(`/api/cms/doctors/${id}`, { locale });
+
+		return doctor;
 	}
 }
